fce6471a554c8c6549f7107ad2bfb7bb0d25513c
Sudhish Sepaul <sudhish.sepaul@gmail.com>
2022-04-12 11:26:55 -0500

    added update to Readme.md

---
+++ migrations/versions/7ad42f6f8999_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7ad42f6f8999
+Revises: bfb7a023f8c9
+Create Date: 2022-04-12 11:07:02.223826
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7ad42f6f8999'
+down_revision = 'bfb7a023f8c9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
